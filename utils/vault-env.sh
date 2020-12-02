8184917d055fadd476e42109b721fb489af8a0e1
Matthias Larsen <mlarsen@gammamedia.org>
2020-12-02 23:30:17 +0100

    feat: Vault support
    Signed-off-by: Matthias Larsen <mlarsen@gammamedia.org>

---
+++ utils/vault-env.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ -d "/vault" ]]
+then
+    echo "Vault present, loading env variables"
+    for f in /vault/secrets/*; do
+       echo "Loading: $f"
+#       source $f
+       export $(grep -v '^#' $f | xargs)
+    done
+else
+    echo "Vault not present"
+fi
